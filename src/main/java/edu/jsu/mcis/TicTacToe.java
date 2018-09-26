a0a969b41ab15177b5b7ea6cf97ef26d999a4dc4
Dinorah Bernardini <dbernardini@stu.jsu.edu>
2018-09-25 19:10:40 -0500

    Finished Part 2

--- src/main/java/edu/jsu/mcis/TicTacToe.java
+++ src/main/java/edu/jsu/mcis/TicTacToe.java
@@ -1,5 +1,9 @@
 package edu.jsu.mcis;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class TicTacToe {
 
     private static final int DEFAULT_WIDTH = 3;
@@ -25,22 +29,17 @@ public class TicTacToe {
 
         TicTacToeModel model = new TicTacToeModel(width);
         TicTacToeView view = new TicTacToeView(model);
-        TicTacToeController controller = new TicTacToeController(model, view);
-        
-        /* MAIN LOOP */
-
-        while (!model.isGameover()) {
-            
-            view.viewModel();
-            controller.controlModel();
-            
-        }
+        JFrame win = new JFrame("Tic-Tac-Toe");
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.add(view);
+        win.pack();
+        win.setVisible(true);
         
         /* Game is over; show the final board and the winner */
 
-        view.viewModel();
+        // view.viewModel();
 
-        view.showResult(model.getResult().toString());
+        //view.showResult(model.getResult().toString());
 
     }
 
--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -1,77 +1,90 @@
 package edu.jsu.mcis;
 
-import edu.jsu.mcis.TicTacToeModel.Mark;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class TicTacToeView {
-
-    private TicTacToeModel model;
+public class TicTacToeView extends JPanel implements ActionListener {
     
-    /* CONSTRUCTOR */
-	
+    TicTacToeModel model;
+
+    private JButton[][] squares;
+    private JPanel squaresPanel;
+    private JLabel resultLabel;
+
     public TicTacToeView(TicTacToeModel model) {
-        
+
         this.model = model;
-        
-    }
-	
-    public void viewModel() {
-        
-        /* Print the board to the console (see examples) */
-        
-        /* INSERT YOUR CODE HERE */
-        //nested for loop
-        //nested for loop bc i have rows and columns 
 
-        System.out.print("\n\n");
-        
-        System.out.print("  ");
+        int width = model.getWidth();
 
-        for(int i = 0; i < model.getWidth(); i++) {
-            System.out.print(i);
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        squares = new JButton[width][width];
+        squaresPanel = new JPanel(new GridLayout(width,width));
+        resultLabel = new JLabel();
+        resultLabel.setName("ResultLabel");
+        
+        for (int row = 0; row < width; row++){
+            
+            for (int col = 0; col < width; col++){
+                
+                squares[row][col] = new JButton(); 
+                squares[row][col].addActionListener(this);
+                squares[row][col].setName("Square" + row + col);
+                squares[row][col].setPreferredSize(new Dimension(64,64));
+                squaresPanel.add(squares[row][col]);
+                
+            }
+            
         }
+    
+        this.add(squaresPanel);
+        this.add(resultLabel);
         
-        System.out.print("\n\n");
-
-        for(int i = 0; i < model.getWidth(); i++) {
-            System.out.print(i + " ");
-                for(int j=0; j < model.getWidth(); j++)
-                System.out.print(model.getMark(i,j));
-                System.out.print("\n");
-        }
-            System.out.print("\n\n");
+        resultLabel.setText("Welcome to Tic-Tac-Toe!");
     }
 
-    public void showNextMovePrompt() {
-
-        /* Display a prompt for the player's next move (see examples) */
-
-        /* INSERT YOUR CODE HERE */ 
-        if( model.isXTurn() ){
-        System.out.println("Player 1 (X) Move: " + 
-        "\nEnter the row and column numbers, separated by a space: "); 
-        } else {
-        System.out.println("Player 2 (O) Move: " + 
-        "\nEnter the row and column numbers, separated by a space: "); 
-        }
-    }
-
-    public void showInputError() {
-
-        /* Display an error if input is invalid (see examples) */
-
-        /* INSERT YOUR CODE HERE */ 
-
-        System.out.println("Error: Input is Invalid"); 
+    @Override
+    public void actionPerformed(ActionEvent event) {
         
+        /* Handle button clicks.  
+        //Extract the row and col values from the name of the button that was clicked, 
+        //*then make the mark in the grid using the Model's "makeMark()" method.  
+        //*Finally, use the "updateSquares()" method to refresh the View.  
+        // If the game is over, show the result (from the Model's "getResult()" method) in the result label. */
+        
+        String name = ((JButton) event.getSource()).getName(); // Get button name
+        
+        // INSERT YOUR CODE HERE
+
+        int row = Integer.parseInt(name.substring(6,7));
+        int col = Integer.parseInt(name.substring(7,8));
+        model.makeMark(row, col);
+        updateSquares();
+        
+        //game over
+        if(model.isGameover()) {
+            showResult(model.getResult().toString().toUpperCase());
+		}
+
+    
+    }
+        
+    public void updateSquares() {
+
+        /* Loop through all View buttons and (re)set the text of each button
+           to reflect the grid contents (use the Model's "getMark()" method). */
+
+           for( int i = 0; i < model.getWidth(); i++){
+               for( int j = 0; j < model.getWidth(); j++){
+                   squares[i][j].setText(model.getMark(i, j).toString());
+               }
+           }
+
+    }
+        
+    public void showResult(String message) {
+        resultLabel.setText(message.toUpperCase());
     }
 
-
-    public void showResult(String r) {
-
-        /* Display final winner */
-
-        System.out.println(r + "!");
-
-    }
-	
 }
